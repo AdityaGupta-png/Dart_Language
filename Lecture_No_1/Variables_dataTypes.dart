5fde8ce54c49cc045f2c1b32b9acdc15f9ff12c5
Aditya <adityagupta64387@gmail.com>
2026-07-27 21:14:42 +0530

    Learning of Number Datatype

---
+++ Lecture_No_1/Variables_dataTypes.dart
@@ -0,0 +1,15 @@
+// Variables --> Anything that store the values
+void main() {
+
+  // For the Numbers --> DATATYPE 
+  int number1 = 60;
+  double number2 = 77.33;
+  double number3 = 33;
+  num number4 = 12;
+  num number5 = 12.44;
+  print(number1);
+  print(number2);
+  print(number3);
+  print(number4);
+  print(number5);
+}
