0fb17e38678dd1e3ee8f6a7cb02aa2cd4b15eaaf
Aditya <adityagupta64387@gmail.com>
2026-07-28 08:34:41 +0530

    Make the pakage of the Variable and datatype and add all datatype on it that i am learing through out the course

---
+++ Lecture_No_1/Variable_And_DataType/Numbers_Types.dart
@@ -0,0 +1,15 @@
+// Variables --> Anything that store the values
+void main() {
+  // For the Numbers --> DATATYPE
+  int number1 = 60;
+  double number2 = 77.33;
+  double number3 = 33;
+  num number4 = 12;
+  num number5 = 12.44;
+
+  print("Integer Number ${number1}");
+  print("Double Number ${number2}");
+  print("Double Number ${number3}");
+  print("Num Number ${number4}");
+  print("Num Number ${number5}");
+}
